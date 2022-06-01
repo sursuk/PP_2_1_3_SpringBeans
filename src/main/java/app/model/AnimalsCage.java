4be7900cad86f13521816f1dcc49cc730765c104
sursuk <sursuk@users.noreply.github.com>
2022-06-01 03:12:15 +0300

    Версия 1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,20 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+    private Timer timer = new Timer();
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,8 +1,10 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("cat")
 public class Cat extends Animal {
     @Override
     public String toString() {
